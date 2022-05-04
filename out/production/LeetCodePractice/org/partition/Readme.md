2e6c250118f8a9d58daa93ce546f7fc0263404f6
Second Chen <second_chen@trendmicro.com>
2022-05-04 20:14:37 +0800

    add partition

---
+++ out/production/LeetCodePractice/org/partition/Readme.md
@@ -0,0 +1,2 @@
+### 分治
+https://github.com/CyC2018/CS-Notes/blob/master/notes/Leetcode%20%E9%A2%98%E8%A7%A3%20-%20%E5%88%86%E6%B2%BB.md
---
+++ src/org/partition/UniqueBinarySearchTrees.java
@@ -0,0 +1,108 @@
+package org.partition;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+public class UniqueBinarySearchTrees {
+    public static void main(String[] args) {
+        List<TreeNode> res = generateTrees(2);
+        //  1  null 2  null null
+        //  2  1  null null null
+        print(res);
+    }
+
+    private static void print(List<TreeNode> res) {
+        for (TreeNode node: res) {
+            printTree(node);
+            System.out.println();
+        }
+    }
+
+    private static void printTree(TreeNode node) {
+        if(node==null){
+            System.out.print(" "+"null");
+            return;
+        }
+        System.out.print(" "+node.val+" ");
+        printTree(node.left);
+        printTree(node.right);
+    }
+
+    public static List<TreeNode> generateTrees(int n) {
+        if (n < 1) {
+            return new LinkedList<>();
+        }
+        return generateSubtrees(1, n);
+    }
+
+    private static List<TreeNode> generateSubtrees(int s, int e) {
+        List<TreeNode> res = new LinkedList<>();
+        if (s > e) {
+            res.add(null);
+            return res;
+        }
+        for (int i = s; i <= e; ++i) {
+            List<TreeNode> leftSubtrees = generateSubtrees(s, i - 1);
+            List<TreeNode> rightSubtrees = generateSubtrees(i + 1, e);
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+
+//    public static List<TreeNode> generateTrees(int n) {
+//        if (n < 1) {
+//            return new LinkedList<>();
+//        }
+//        return generateTrees(1, n);
+//    }
+//
+//    private static List<TreeNode> generateTrees(int start, int end) {
+//        List<TreeNode> res = new LinkedList<>();
+//        if (start > end) {
+//            res.add(null);
+//            return res;
+//        }
+//
+//        for (int i = start; i <= end; i++) {
+//            List<TreeNode> leftTree = generateTrees(start, i - 1);
+//            List<TreeNode> rightTree = generateTrees(i + 1, end);
+//            for (TreeNode l : leftTree) {
+//                for (TreeNode r : rightTree) {
+//                    TreeNode node = new TreeNode(i);
+//                    node.left = l;
+//                    node.right = r;
+//                    res.add(node);
+//                }
+//            }
+//        }
+//        return res;
+//    }
+}
